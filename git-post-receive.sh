1750187955b746754a31387355e730560cd13b68
Guo Du <mrduguo@duguo.org>
2012-01-14 22:52:58 +0000

    test

--- git-post-receive.sh
+++ git-post-receive.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 git --work-tree /etc/puppet checkout -f
-sudo puppet apply /etc/puppet/manifests/site.pp --modulepath=/etc/puppet/modules
+sudo puppet apply -v /etc/puppet/manifests/site.pp --modulepath=/etc/puppet/modules
