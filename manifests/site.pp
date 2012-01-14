4cc1a2d17aaecea010eaba8b8e441fbc048ab5c9
Guo Du <mrduguo@duguo.org>
2012-01-14 22:58:09 +0000

    test

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,31 +1,11 @@
-class helloworld {
-   info("hello world from  $environment $hostname")
-   host{"foo.bar":
-      ip => "127.0.0.7",
-   }
-   augeas { "sshd_config":
-	  changes => [
-	    "set /files/etc/ssh/sshd_config/PermitRootLogin yes",
-	  ],
-	}
-
-}
-
-class helloworld::again {
-   info("hello world again")
-}
-
 
 node base {
     include screen
-    include helloworld
 }
-
 
 node default inherits base {
-    include helloworld::again
 }
-node xubuntuserver inherits default {
-    include helloworld::again
-    info("for ubuntu server")
+
+node ubuntuserver inherits default {
+    info("for ubuntuserver vm")
 }
