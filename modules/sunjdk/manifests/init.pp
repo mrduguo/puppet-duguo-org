b83da79bdd0b7956d7d1ed6dbba6e3bd1a426b92
Guo Du <mrduguo@duguo.org>
2012-01-15 02:02:04 +0000

    test

--- modules/sunjdk/manifests/init.pp
+++ modules/sunjdk/manifests/init.pp
@@ -3,6 +3,5 @@ class sunjdk {
       path => "/usr/bin:/usr/sbin:/bin",
   	  command => "apt-get -y install sun-java6-jdk",
   	  creates => "/usr/lib/jvm/java-6-sun/jre/bin/java",
-  	  returns => [100,0],
 	}
 }
