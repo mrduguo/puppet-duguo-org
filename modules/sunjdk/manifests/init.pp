8b62bde3b79ec12f9b24c4d727bd6e668935356e
Guo Du <mrduguo@duguo.org>
2012-01-15 12:46:19 +0000

    test

--- modules/sunjdk/manifests/init.pp
+++ modules/sunjdk/manifests/init.pp
@@ -1,29 +1,4 @@
 class sunjdk {
-	exec { "install-sunjdk1":
-      path => "/usr/bin:/usr/sbin:/bin",
-      logoutput => true,
-  	  command => "apt-get -y install python-software-properties"
-	}
-	
-	exec { "install-sunjdk2":
-      path => "/usr/bin:/usr/sbin:/bin",
-      logoutput => true,
-  	  command => "add-apt-repository -y ppa:ferramroberto/java"
-	}
-	
-	exec { "install-sunjdk3":
-      path => "/usr/bin:/usr/sbin:/bin",
-      logoutput => true,
-  	  command => "apt-get update"
-	}
-
-	
-	exec { "install-sunjdk5":
-      path => "/usr/bin:/usr/sbin:/bin",
-      logoutput => true,
-  	  command => "echo sun-java6-jre shared/accepted-sun-dlj-v1-1 select true | /usr/bin/debconf-set-selections"
-	}
-	
 	exec { "install-sunjdk6":
       path => "/usr/bin:/usr/sbin:/bin",
       logoutput => true,
