35e2b6065ad245eaed4f77be8b0765a32fcf2932
Guo Du <mrduguo@duguo.org>
2012-01-15 01:54:58 +0000

    test

--- modules/sunjdk/manifests/init.pp
+++ modules/sunjdk/manifests/init.pp
@@ -1,7 +1,7 @@
 class sunjdk {
 	exec { "install-sunjdk":
       path => "/usr/bin:/usr/sbin:/bin",
-  	  command => "echo 'sun-java6-jre shared/accepted-sun-dlj-v1-1 select true' | /usr/bin/debconf-set-selections && apt-get -y install sun-java6-jdk",
+  	  command => "apt-get -y install sun-java6-jdk",
   	  creates => "/usr/lib/jvm/java-6-sun/jre/bin/java"
 	}
 }
