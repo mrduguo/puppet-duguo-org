54cd86077d94e7ba699aac6bb439f26f4e7e641b
Guo Du <mrduguo@duguo.org>
2012-01-15 12:32:07 +0000

    test

--- modules/sunjdk/manifests/init.pp
+++ modules/sunjdk/manifests/init.pp
@@ -1,7 +1,33 @@
 class sunjdk {
-	exec { "install-sunjdk":
+	exec { "install-sunjdk1":
+      path => "/usr/bin:/usr/sbin:/bin",
+  	  command => "apt-get -y install python-software-properties"
+	}
+	
+	exec { "install-sunjdk2":
+      path => "/usr/bin:/usr/sbin:/bin",
+  	  command => "add-apt-repository -y ppa:ferramroberto/java"
+	}
+	
+	exec { "install-sunjdk3":
+      path => "/usr/bin:/usr/sbin:/bin",
+  	  command => "apt-get update"
+	}
+	
+	exec { "install-sunjdk4":
       path => "/usr/bin:/usr/sbin:/bin",
   	  command => "apt-get -y install python-software-properties && add-apt-repository -y ppa:ferramroberto/java && apt-get update &&  echo sun-java6-jre shared/accepted-sun-dlj-v1-1 select true | /usr/bin/debconf-set-selections && apt-get -y install sun-java6-jdk",
+  	  creates => "/usr/lib/jvm/java-6-sun/jre/bin/java"
+	}
+	
+	exec { "install-sunjdk5":
+      path => "/usr/bin:/usr/sbin:/bin",
+  	  command => "echo sun-java6-jre shared/accepted-sun-dlj-v1-1 select true | /usr/bin/debconf-set-selections"
+	}
+	
+	exec { "install-sunjdk6":
+      path => "/usr/bin:/usr/sbin:/bin",
+  	  command => "apt-get -y install sun-java6-jdk",
   	  creates => "/usr/lib/jvm/java-6-sun/jre/bin/java"
 	}
 }
