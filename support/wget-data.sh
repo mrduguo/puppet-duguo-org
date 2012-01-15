1469dcfa2f99b8ec69e102a2c80398efa91d1299
Guo Du <mrduguo@duguo.org>
2012-01-15 02:27:03 +0000

    test

--- support/wget-data.sh
+++ support/wget-data.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -ex
+# Usage:
 #
 # wget -qO- https://raw.github.com/mrduguo/puppet-duguo-org/master/support/wget-data.sh | /bin/bash
 #
 
-wget -qOpuppet-git-init.pp https://raw.github.com/mrduguo/puppet-duguo-org/master/support/puppet-git-init.pp
-
 apt-get -y install puppet
 rm -rf /etc/puppet/* 
 
+wget -qOpuppet-git-init.pp https://raw.github.com/mrduguo/puppet-duguo-org/master/support/puppet-git-init.pp
 puppet apply -v puppet-git-init.pp
-
 rm -f puppet-git-init.pp
+
