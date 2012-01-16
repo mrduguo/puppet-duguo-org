6da7214465c2d999ebc3f7f323a01569696eda21
Guo Du <mrduguo@duguo.org>
2012-01-16 23:16:32 +0000

    test

--- modules/xdir/manifests/init.pp
+++ modules/xdir/manifests/init.pp
@@ -1,6 +1,14 @@
 class xdir {
 
 	include params
+		$softwareVersion = "0.9.0"
+    	$softwareHome = "/opt/xdir"
+    	$softwarePathUnpack = "/opt/xdir/dist"
+    	$softwarePathCurrent = "/opt/xdir/CURRENT"
+    	$softwarePathData = "/opt/xdir/data"
+    	$softwarePathVar = "/opt/xdir/var"
+    	$softwareDownloadUrl = "http://mrduguo.github.com/maven/repos/release/org/duguo/xdir/dist/xdir-dist-bin/${softwareVersion}/xdir-dist-bin-${softwareVersion}.tar.gz"
+
 
 	stage { "pre": before => Stage["main"] }
 	class { "sunjdk" : stage => pre }
@@ -23,7 +31,7 @@ class xdir {
 
 
     exec { "install-xdir" :
-        command => "wget -qO- $softwareDownloadUrl | tar -xzf - -C /opt/xdir/dist",
+        command => "wget -qO- ${softwareDownloadUrl} | tar -xzf - -C /opt/xdir/dist",
         creates => "/opt/xdir/dist/xdir-dist-bin-${softwareVersion}",
         logoutput => true,
         require => File["/opt/xdir/dist"],
