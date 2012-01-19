e7625a4d4c88aaebd31f774c624c2cd72e6c7983
Guo Du <mrduguo@duguo.org>
2012-01-19 22:42:12 +0000

    test

--- modules/xdir/manifests/params.pp
+++ modules/xdir/manifests/params.pp
@@ -1,5 +1,5 @@
 class xdir::params {
-	$softwareVersion = "0.9.0"
+	$softwareVersion = "0.9.1"
 	$softwareHome = "/opt/xdir"
 	$softwareDownloadUrl = "http://mrduguo.github.com/maven/repos/release/org/duguo/xdir/dist/xdir-dist-bin/${softwareVersion}/xdir-dist-bin-${softwareVersion}.tar.gz"
 }
