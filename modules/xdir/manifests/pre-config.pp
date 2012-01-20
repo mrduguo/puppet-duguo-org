f032059aa993eb26942f9e55dc725d86689fcb1a
Guo Du <mrduguo@duguo.org>
2012-01-20 23:36:16 +0000

    test

--- modules/xdir/manifests/pre-config.pp
+++ modules/xdir/manifests/pre-config.pp
@@ -4,11 +4,11 @@ class xdir::pre-config {
 
 	include sunjdk
 
-	include iptables
-
 	iptables::rule{"foward-port-80-to-8080":
 	    rule => '-t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 8080',
 	}
+
+	include iptables
 
     exec { "shutdown-xdir" :
       command => "${xdir::params::softwareHome}/CURRENT/bin/xdir.sh stop",
