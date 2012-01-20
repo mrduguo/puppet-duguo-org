a23f34405ff89c71cd1b0c7141c73911273070bc
Guo Du <mrduguo@duguo.org>
2012-01-20 22:54:01 +0000

    test

--- modules/xdir/manifests/pre-config.pp
+++ modules/xdir/manifests/pre-config.pp
@@ -6,6 +6,10 @@ class xdir::pre-config {
 
 	include iptables
 
+	iptables::rule{"foward-port-80-to-8080":
+	    rule => '-t nat -A PREROUTING -p tcp --dport 80 -j REDIRECT --to 8080',
+	}
+
     exec { "shutdown-xdir" :
       command => "${xdir::params::softwareHome}/CURRENT/bin/xdir.sh stop",
       onlyif => "test -f ${xdir::params::softwareHome}/CURRENT/bin/xdir.sh",
