fd67ee16938a228d6a171790e7c746cf4f593e6c
Guo Du <mrduguo@duguo.org>
2012-01-22 23:00:49 +0000

    test

--- manifests/nodes/ubuntuserver.pp
+++ manifests/nodes/ubuntuserver.pp
@@ -2,6 +2,6 @@ node ubuntuserver inherits default {
     info("for ubuntuserver vm")
     host{
         name => "${hostname}",
-        ip => "192.168.56.101"
+        ip => "192.168.56.101",
     }
 }
